11ca83ccaf5a2d82e9afd37b466236058e4bce87
minro <5997507+minro@users.noreply.github.com>
2023-04-06 16:43:52 +0200

    prep for transfer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,10 @@ import type { AppProps } from 'next/app'
 import AzureAppInsights from "../utils/insights"
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <AzureAppInsights>
-      <Component {...pageProps} />
-    </AzureAppInsights>
-  )
+  // Azure Application insights, also add .env with NEXT_PUBLIC_APP_INSIGHTS
+  // return (
+  //   <AzureAppInsights>
+  //     <Component {...pageProps} />
+  //   </AzureAppInsights>
+  // )
 }
